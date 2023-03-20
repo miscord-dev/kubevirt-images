49650204ed02e4400d21325937d428a27f1fd957
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2023-03-20 13:27:35 +0900

    Debug

--- fcos.js
+++ fcos.js
@@ -9,6 +9,8 @@
 
     await $`docker run --platform=linux/amd64 --security-opt label=disable --pull=always --rm -v ${pwd}:/data -w /data quay.io/coreos/coreos-installer:release download -f iso`
     
+    await $`ls`
+
     await $`bash -c "mv *.iso image.img"`
 
     await $`docker build -t fcos -f ../Dockerfile .`
